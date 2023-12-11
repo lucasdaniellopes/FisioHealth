d61997a6cb6449683bcbfa379be57fc3e18abaf8
lucasdaniellopes <lucasdanielspead@gmail.com>
2023-12-11 11:40:44 -0300

    adicionado comentarios de todo o codigo

--- pages/homepage/homepage.js
+++ pages/homepage/homepage.js
@@ -1,27 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Seleciona o container do carrossel e as imagens dentro dele
   const carrossel = document.querySelector(".content_carrossel_container");
   const images = document.querySelector(".content_carrossel_images");
 
+  // Obtém a largura de uma imagem no carrossel
   let imageWidth = document.querySelector(
     ".content_carrossel_images img"
-  ).clientWidth; /* Obtém a largura de uma imagem  */
-  let indexImage = 0; /* Acompanha o índice da imagem atual no carrossel */
+  ).clientWidth;
 
+  // Acompanha o índice da imagem atual no carrossel
+  let indexImage = 0;
+
+  // Função para avançar para a próxima imagem no carrossel
   function goImages() {
     indexImage++;
 
+    // Volta para a primeira imagem quando atinge a última
     if (indexImage >= images.children.length) {
-      indexImage = 0; /* Volta para a primeira imagem quando atinge a última */
+      indexImage = 0;
     }
 
-    updateCarrossel(); /* Atualiza a posição do carrossel */
+    // Atualiza a posição do carrossel para mostrar a próxima imagem
+    updateCarrossel();
   }
 
+  // Função para atualizar a posição do carrossel
   function updateCarrossel() {
-    images.style.transform = `translateX(${
-      -imageWidth * indexImage
-    }px)`; /* Move o carrossel horizontalmente para mostrar a próxima imagem */
+    // Move o carrossel horizontalmente para mostrar a próxima imagem
+    images.style.transform = `translateX(${-imageWidth * indexImage}px)`;
   }
 
-  setInterval(goImages, 3000); /* Avança automaticamente a cada 3 segundos */
+  // Configura um intervalo para avançar automaticamente a cada 3 segundos
+  setInterval(goImages, 3000);
 });
--- pages/register/register.js
+++ pages/register/register.js
@@ -1,11 +1,21 @@
-function showFields(){
-  document.querySelector(".typeOfUser_box").addEventListener("click", (event)=>{
-    if (event.target.value != undefined && event.target.value === "user"){
-      document.querySelector(".profissionalInfo_box").classList.add("hidden")
-    }
-    else{
-      document.querySelector(".profissionalInfo_box").classList.remove("hidden")
-    }
-  })
+// Função para mostrar ou ocultar campos com base na seleção do tipo de usuário
+function showFields() {
+  // Adiciona um ouvinte de evento de clique à caixa de tipo de usuário
+  document
+    .querySelector(".typeOfUser_box")
+    .addEventListener("click", (event) => {
+      // Verifica se o valor do elemento clicado não é undefined e é do tipo "user"
+      if (event.target.value != undefined && event.target.value === "user") {
+        // Adiciona a classe "hidden" ao campo profissionalInfo_box se o tipo de usuário for "user"
+        document.querySelector(".profissionalInfo_box").classList.add("hidden");
+      } else {
+        // Remove a classe "hidden" do campo profissionalInfo_box se o tipo de usuário não for "user"
+        document
+          .querySelector(".profissionalInfo_box")
+          .classList.remove("hidden");
+      }
+    });
 }
-showFields()
+
+// Chama a função para que ela seja executada quando a página carregar
+showFields();
